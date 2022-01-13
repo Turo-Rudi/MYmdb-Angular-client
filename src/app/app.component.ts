d4e36d137c6ad26ff9c51ba05326acc8a5a75e32
Turo-Rudi <jozsa.andrea@gmail.com>
2022-01-13 23:51:37 +0100

    delete unnecessary code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,11 @@
 import { Component } from '@angular/core';
-import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
-import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
-import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'MYmdb-Angular-client';
-
-  constructor(public dialog: MatDialog) { }
-  //this function opens the dialog when the sign up button is clicked
-  openUserRegistrationDialog(): void {
-    this.dialog.open(UserRegistrationFormComponent, {
-      width: '280px'
-    });
-  }
-
-  openUserLoginDialog(): void {
-    this.dialog.open(UserLoginFormComponent, {
-      width: '280px'
-    });
-  }
 }
