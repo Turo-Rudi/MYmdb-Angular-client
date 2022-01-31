b48551c2edaf3884c1e7ac2d31b7f8325315a4e5
Turo-Rudi <jozsa.andrea@gmail.com>
2022-01-31 23:10:10 +0100

    add userLogout

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
@@ -7,7 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public snackBar: MatSnackBar,
+    public router: Router
+  ) { }
+
+  userLogout(): void {
+    localStorage.clear();
+    this.snackBar.open('You successfully logged out!', 'OK', {
+      duration: 2000,
+    });
+    this.router.navigate(['/welcome']).then(() => {
+      window.location.reload();
+    });
+  }
 
   ngOnInit(): void {
   }
